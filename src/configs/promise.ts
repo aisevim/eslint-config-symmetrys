aaf45151f1cede6c6200a0aa840e264f166298b8
aisevim <a.aydnlp.pro@gmail.com>
2023-11-25 18:09:23 +0100

    feat(config): create promise config

---
+++ src/configs/promise.ts
@@ -0,0 +1,20 @@
+import promise from 'eslint-plugin-promise'
+
+export async function promise() {
+	return {
+		files: ["**/*.?([cm])[jt]s?(x)"],
+		plugins: {
+			promise
+		},
+		rules: {
+			'promise/no-callback-in-promise': 'error',
+			'promise/no-multiple-resolved': 'error',
+			'promise/no-nesting': 'error',
+			'promise/no-new-statics': 'error',
+			'promise/no-return-in-finally': 'error',
+			'promise/no-return-wrap': 'error',
+			'promise/prefer-await-to-then': 'error',
+			'promise/valid-params': 'error',
+		}
+	}
+}
