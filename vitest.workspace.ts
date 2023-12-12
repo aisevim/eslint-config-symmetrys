b345bedfb46e6ad6cc6e6494aa3e5f1d0b08c307
aisevim <a.aydnlp.pro@gmail.com>
2023-12-12 23:00:45 +0100

    refactor: wording on test workflow and vitest workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,7 +3,7 @@ import { defineWorkspace } from 'vitest/config'
 export default defineWorkspace([
   {
     test: {
-      name: 'root',
+      name: 'default - default-format - vue-ts - vue-ts-format',
       include: ['tests/**/*.{test,spec}.{ts,js,tsx}'],
       environment: 'node',
     },
