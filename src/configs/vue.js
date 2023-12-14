94518688f764356b67ece7b5c7734d39780bc04f
aisevim <a.aydnlp.pro@gmail.com>
2023-12-14 14:29:33 +0100

    fix: delete ts-support on vue file (mistake)

--- src/configs/vue.js
+++ src/configs/vue.js
@@ -1,21 +1,14 @@
-import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import vuePlugin from 'eslint-plugin-vue'
 import vueParser from 'vue-eslint-parser'
 
 import { VUE_GLOB } from '../constants.js'
-import { getTsConfigOptions } from './typescript.js'
 
-
-export async function vue({ options }) {
-  const isTs = options?.ts
-  const tsConfigFileOptions = isTs ? getTsConfigOptions(options?.ts?.project) : {}
-
+export async function vue() {
   return {
     files: [VUE_GLOB],
     plugins: {
       vue: vuePlugin,
-      ...(isTs ? { ts: tsPlugin } : {}),
     },
     processor: vuePlugin.processors['.vue'],
     languageOptions: {
@@ -28,15 +21,12 @@ export async function vue({ options }) {
         ecmaFeatures: {
           jsx: true,
         },
-        ...tsConfigFileOptions?.parserOptions,
       },
     },
     rules: {
       ...vuePlugin.configs['vue3-essential'].rules,
       ...vuePlugin.configs['vue3-strongly-recommended'].rules,
       ...vuePlugin.configs['vue3-recommended'].rules,
-
-      ...tsConfigFileOptions?.rules,
     },
   }
 }
