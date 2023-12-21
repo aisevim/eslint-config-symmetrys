863991cede7862ff73101d4aca6a1805eb2d1b48
aisevim <a.aydnlp.pro@gmail.com>
2023-12-21 22:34:13 +0100

    fix(ts): add adapted ts rules

--- src/configs/typescript.js
+++ src/configs/typescript.js
@@ -63,6 +63,29 @@ export async function typescriptConfig({ options = {}, extensions = [], settings
       rules: {
         ...renameRules(pluginTS.configs['eslint-recommended'].overrides[0].rules, '@typescript-eslint/', 'ts/'),
         ...renameRules(pluginTS.configs.strict.rules, '@typescript-eslint/', 'ts/'),
+
+        'no-loss-of-precision': 'off',
+        'no-redeclare': 'off',
+        'no-use-before-define': 'off',
+        'no-useless-constructor': 'off',
+        'no-unused-vars': 'off',
+        'ts/no-explicit-any': 'off',
+        'ts/no-dynamic-delete': 'off',
+        'ts/no-extraneous-class': 'off',
+        'ts/no-invalid-void-type': 'off',
+        'ts/no-non-null-assertion': 'off',
+        'ts/no-useless-constructor': 'off',
+        'ts/triple-slash-reference': 'off',
+        'ts/unified-signatures': 'off',
+
+        'ts/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+        'ts/ban-types': ['error', { types: { Function: false } }],
+        'ts/consistent-type-imports': ['error', { disallowTypeAnnotations: false, prefer: 'type-imports' }],
+        'ts/no-import-type-side-effects': 'error',
+        'ts/no-loss-of-precision': 'error',
+        'ts/no-redeclare': 'error',
+        'ts/no-require-imports': 'error',
+
         ...parserOptionsConfig?.rules,
       },
     }),
@@ -72,6 +95,7 @@ export async function typescriptConfig({ options = {}, extensions = [], settings
         'eslint-comments/no-unlimited-disable': 'off',
         'import/no-duplicates': 'off',
         'no-restricted-syntax': 'off',
+        'ts/no-unused-vars': 'off',
       },
     },
     {
